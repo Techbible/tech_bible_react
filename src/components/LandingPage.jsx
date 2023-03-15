06bccaec56d3dc79c16a5b3c49128c377fd0b679
Zorous <oussamabelhadi5@gmail.com>
2023-03-15 17:33:03 +0100

    init

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,187 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function LandingPage() {
+  return (
+    <div>
+    
+<div className="desktop-1-nEq">
+  <img alt="pic" className="vector-3-4zy" src="REPLACE_IMAGE:2:27"/>
+  <p className="news-7TT">News</p>
+  <p className="discover-more-8dT">Discover more</p>
+  <img alt="pic" className="vector-53-i5s" src="/assets/vector-53.png"/>
+  <p className="app-of-the-day-kYM">App of the Day</p>
+  <div className="rectangle-78-gaR">
+  </div>
+  <div className="rectangle-79-xH3">
+  </div>
+  <div className="rectangle-73-r7X">
+  </div>
+  <img alt="pic" className="vector-55-jyw" src="REPLACE_IMAGE:2:83"/>
+  <img alt="pic" className="vector-54-r2y" src="REPLACE_IMAGE:2:81"/>
+  <p className="trendy-posts-of-the-week-m9w">
+    <span className="trendy-posts-of-the-week-m9w-sub-0">- </span>
+    <span className="trendy-posts-of-the-week-m9w-sub-1">Trendy posts of the week</span>
+  </p>
+  <p className="slack-boosts-ai-capabilities-with-new-chatgpt-app-YM3">
+  Slack boosts AI capabilities with new ChatGPT app
+  <br/>
+  
+  </p>
+  <p className="government-backed-digital-money-to-represent-213b-in-payments-by-2030-aoX">Government-backed digital money to represent $213B in payments by 2030</p>
+  <p className="how-many-jobs-are-available-in-technology-in-the-us-qDf">How many jobs are available in technology in the US?</p>
+  <p className="news-letter-W4u">News Letter</p>
+  <p className="get-the-latest-saas-products-news-directly-to-your-inbox-no7">Get the latest Saas products news directly to your inbox!</p>
+  <p className="adobe-suites-3j3">Adobe Suites</p>
+  <p className="browse-1000-of-the-latest-tech-tools-per-task-updated-daily-j65">Browse 1000+ of the latest tech tools per task Updated daily</p>
+  <p className="minecraft-Mt9">Minecraft</p>
+  <p className="browse-1000-of-the-latest-tech-tools-per-task-updated-daily-NYM">Browse 1000+ of the latest tech tools per task Updated daily</p>
+  <p className="python-m4h">Python</p>
+  <p className="browse-1000-of-the-latest-tech-tools-per-task-updated-daily-Eys">Browse 1000+ of the latest tech tools per task Updated daily</p>
+  <p className="cheers--Jyj">“Cheers !”</p>
+  <p className="published-by-r-rita64-2-hours-ago-Mh7">
+    <span className="published-by-r-rita64-2-hours-ago-Mh7-sub-0">Published by </span>
+    <span className="published-by-r-rita64-2-hours-ago-Mh7-sub-1">r/Rita64 -</span>
+    <span className="published-by-r-rita64-2-hours-ago-Mh7-sub-2"> 2 hours ago </span>
+  </p>
+  <img alt="pic" className="vector-57-P1X" src="/assets/vector-57.png"/>
+  <p className="chat-gpt-4tM">Chat GPT</p>
+  <p className="browse-1000-of-the-latest-tech-tools-per-task-updated-daily-xTw">Browse 1000+ of the latest tech tools per task Updated daily</p>
+  <p className="adobe-xd-2ih">Adobe XD</p>
+  <p className="browse-1000-of-the-latest-tech-tools-per-task-updated-daily-X9f">Browse 1000+ of the latest tech tools per task Updated daily</p>
+  <p className="canva-77s">Canva</p>
+  <p className="browse-1000-of-the-latest-tech-tools-per-task-updated-daily-j9F">Browse 1000+ of the latest tech tools per task Updated daily</p>
+  <p className="behance-zay">Behance</p>
+  <p className="browse-1000-of-the-latest-tech-tools-per-task-updated-daily-FG1">Browse 1000+ of the latest tech tools per task Updated daily</p>
+  <p className="figma-7p1">Figma</p>
+  <p className="browse-1000-of-the-latest-tech-tools-per-task-updated-daily-DMF">Browse 1000+ of the latest tech tools per task Updated daily</p>
+  <p className="you-might-also-like-UHB">YOU MIGHT ALSO LIKE</p>
+  <p className="community-wgZ">Community</p>
+  <p className="ask-questions-pkM">Ask Questions</p>
+  <div className="rectangle-89-VrV">
+  </div>
+  <p className="subscribe-to-news-letter-149">Subscribe to news letter</p>
+  <div className="rectangle-90-S9T">
+  </div>
+  <div className="your-email-CH3">Your Email</div>
+  <img alt="pic" className="layer1-qau" src="/assets/layer1-Gv1.png"/>
+  <img alt="pic" className="layer1-M3T" src="/assets/layer1-pSR.png"/>
+  <img alt="pic" className="layer1-f49" src="/assets/layer1-8PF.png"/>
+  <img alt="pic" className="layer1-Z9X" src="/assets/layer1-VbK.png"/>
+  <img alt="pic" className="vector-69-4ry" src="REPLACE_IMAGE:16:327"/>
+  <img alt="pic" className="vector-70-BRo" src="/assets/vector-70.png"/>
+  <p className="item-120-6Hs">120</p>
+  <p className="premium-aTw">Premium</p>
+  <p className="design-tool-FKB">Design tool</p>
+  <img alt="pic" className="layer1-885" src="/assets/layer1-LAR.png"/>
+  <p className="item-120-bnM">120</p>
+  <p className="free-26y">Free</p>
+  <p className="design-tool-SRb">Design tool</p>
+  <img alt="pic" className="layer1-toP" src="/assets/layer1-cTo.png"/>
+  <p className="item-120-oQZ">120</p>
+  <p className="free-gUM">Free</p>
+  <p className="portfolio-tool-yTT">Portfolio tool</p>
+  <img alt="pic" className="layer1-TtR" src="/assets/layer1-qEd.png"/>
+  <p className="item-120-mPK">120</p>
+  <p className="free-3rd">Free</p>
+  <p className="design-tool-kFF">Design tool</p>
+  <img alt="pic" className="layer1-SNy" src="/assets/layer1-GKw.png"/>
+  <img alt="pic" className="adobexdccicon-1-k8m" src="/assets/adobexdccicon-1.png"/>
+  <img alt="pic" className="pngwing-1-2MB" src="/assets/pngwing-1.png"/>
+  <img alt="pic" className="behance-logo-vector-1-UDB" src="/assets/behance-logo-vector-1.png"/>
+  <img alt="pic" className="adobecreativecloudrainbowicon-1-W9s" src="/assets/adobecreativecloudrainbowicon-1.png"/>
+  <img alt="pic" className="minecraft-2752120-2284937-1-jYR" src="/assets/minecraft-2752120-2284937-1.png"/>
+  <img alt="pic" className="python-logo-notext-1-oYH" src="/assets/python-logo-notext-1.png"/>
+  <img alt="pic" className="chatgptlogo-1-tZj" src="/assets/chatgptlogo-1.png"/>
+  <img alt="pic" className="canva-logo-1-PmP" src="/assets/canva-logo-1.png"/>
+  <p className="item--tTF">...</p>
+  <p className="published-by-r-imane31-20-hours-ago-xT7">
+    <span className="published-by-r-imane31-20-hours-ago-xT7-sub-0">Published by </span>
+    <span className="published-by-r-imane31-20-hours-ago-xT7-sub-1">r/Imane31 -</span>
+    <span className="published-by-r-imane31-20-hours-ago-xT7-sub-2"> 20 hours ago </span>
+  </p>
+  <p className="how-do-you-improve-productiviy--xE9">“How do you improve productiviy ?”</p>
+  <p className="published-by-r-miro99-20-hours-ago-1y7">
+    <span className="published-by-r-miro99-20-hours-ago-1y7-sub-0">Published by </span>
+    <span className="published-by-r-miro99-20-hours-ago-1y7-sub-1">r/Miro99 -</span>
+    <span className="published-by-r-miro99-20-hours-ago-1y7-sub-2"> 20 hours ago </span>
+  </p>
+  <div className="rectangle-103-7Ps">
+  </div>
+  <p className="visit-aoF">Visit</p>
+  <div className="rectangle-105-Gg5">
+  </div>
+  <p className="visit-kr9">Visit</p>
+  <div className="rectangle-104-FH7">
+  </div>
+  <p className="visit-8Lu">Visit</p>
+  <img alt="pic" className="screen-shot-2023-03-14-at-2021-1-d2m" src="/assets/screen-shot-2023-03-14-at-2021-1-7uf.png"/>
+  <img alt="pic" className="screen-shot-2023-03-14-at-2022-1-Vqf" src="/assets/screen-shot-2023-03-14-at-2022-1-bLy.png"/>
+  <img alt="pic" className="screen-shot-2023-03-14-at-2022-2-nJy" src="/assets/screen-shot-2023-03-14-at-2022-2-BU1.png"/>
+  <p className="item-20-rZj">20</p>
+  <p className="item-30-LE1">30</p>
+  <p className="item-186-DHo">186</p>
+  <p className="item-20-VWD">20</p>
+  <div className="section-1-aGm">
+
+  </div>
+  <p className="title-cUM">The Largest Saas Tools directory</p>
+  <div className="search-bar-J6H">
+  </div>
+
+  
+  <img alt="pic" className="search-icon-o33" src="/assets/search-icon.png"/>
+  <input type="text" id="search-box" placeholder="Search" className="search-gMj" />
+  <img alt="pic" className="filter-button-w2m" src="/assets/filter-button.png"/>
+  <img alt="pic" className="share-link-button-E1s" src="/assets/share-link-button.png"/>
+  <img alt="pic" className="close-button-XWm" src="/assets/close-button.png"/>
+  <p className="browse-1000-of-the-latest-tech-tools-per-task-updated-daily-qnM">Browse 1000+ of the latest tech tools per task Updated daily</p>
+  <img alt="pic" className="chatgptlogo--W7o" src="/assets/chatgptlogo-.png"/>
+  <img alt="pic" className="canva-logo--oMo" src="/assets/canva-logo-.png"/>
+  <img alt="pic" className="adobe-suites-logo-7dP" src="/assets/adobe-suites-logo.png"/>
+
+
+
+  <Link to="/signup"><div className="sign-up-button-boT">Sign up</div></Link>
+  <div className="sign-in-button-zqb">Sign in</div>
+  <p className="community-21b">Community</p>
+  <p className="resources-5Em">Resources</p>
+  <p className="submit-your-tool-xZT">Submit your tool</p>
+  <a href="www.google.com"><p className="all-tools-357">All tools</p></a>
+  <div className="tiktok-logo-WUV">
+    <div className="rectangle-81-pEH">
+    </div>
+    <img alt="pic" className="tiktok-1-KRw" src="/assets/tiktok-1-4Rb.png"/>
+  </div>
+  <div className="tech-bible-logo-373">
+    <p className="tech-bible-J2y">
+    Tech
+    <br/>
+    Bible
+    </p>
+  </div>
+  <div className="group-25-Aqs">
+    <img alt="pic" className="ellipse-4-gZK" src="/assets/ellipse-4-hn5.png"/>
+    <img alt="pic" className="item-32360-1-o89" src="/assets/-nxD.png"/>
+  </div>
+  <div className="group-28-Jqb">
+    <img alt="pic" className="ellipse-4-q4q" src="/assets/ellipse-4-TEu.png"/>
+    <img alt="pic" className="item-32360-1-95X" src="/assets/-Wiq.png"/>
+  </div>
+  <div className="group-25-fJm">
+    <img alt="pic" className="ellipse-4-nPP" src="/assets/ellipse-4.png"/>
+    <img alt="pic" className="item-32360-1-txD" src="/assets/-RF3.png"/>
+  </div>
+  <div className="group-29-ctD">
+    <img alt="pic" className="ellipse-4-97T" src="/assets/ellipse-4-RY9.png"/>
+    <img alt="pic" className="item-32360-1-eK7" src="/assets/-yc9.png"/>
+  </div>
+  <img alt="pic" className="white-youtube-icon-png-28-1-xqb" src="/assets/white-youtube-icon-png-28-1.png"/>
+  <img alt="pic" className="pngfind-1-eCd" src="/assets/pngfind-1.png"/>
+</div>
+
+    </div>
+  )
+}
+
+export default LandingPage
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import "/styles/sign-up.css";
+
+function SignUp() {
+  return (
+    <div className="sign-up-tZj">
+  <div className="auto-group-bcjh-bj3">
+    <div className="auto-group-zen1-Hrm">
+      <img alt="pic" className="eba91235a2dc1787ad14305033cb2e0ec5720x720-2-NdK" src="./assets/eba91235a2dc1787ad14305033cb2e0ec5720x720-2.png"/>
+      <p className="you-already-have-an-account--eqj">You already have an account ?</p>
+      <div className="auto-group-hkyz-JfP">Sign in</div>
+    </div>
+    <img alt="pic" className="vector-69-vRs" src="REPLACE_IMAGE:18:476"/>
+  </div>
+  <div className="auto-group-4ktm-e6y">
+    <div className="rectangle-99-RX3">
+    </div>
+    <p className="by-clicking-on-sign-up-you-agree-to-our-conditions-of-use-and-our-privacy-settings-6NH">By clicking on sign up you agree to our conditions of use and our privacy settings</p>
+    <p className="new-account-XiV">New Account</p>
+    <img alt="pic" className="vector-68-1df" src="REPLACE_IMAGE:14:286"/>
+    <p className="or-sign-up-with-google-W4d">Or Sign up with google</p>
+    <p className="full-name-N6q">Full Name</p>
+    <p className="email-T8H">Email</p>
+    <p className="password-jrV">Password</p>
+    <div className="rectangle-102-Dmf">
+    </div>
+    <p className="sign-up-7s3">Sign Up</p>
+    <div className="rectangle-100-bnD">
+    </div>
+    <div className="rectangle-101-W8V">
+    </div>
+    <div className="rectangle-66-15F">
+    </div>
+    <div className="full-name-J4M">Full Name</div>
+    <div className="characters-minimum-x8u">8 characters minimum</div>
+    <div className="examplesitecom-dVw">Example@site.com</div>
+    <img alt="pic" className="item-1-04-512-1-Wpd" src="./assets/-Z1f.png"/>
+    <img alt="pic" className="item-561127-1-oYq" src="./assets/-9zu.png"/>
+    <img alt="pic" className="iconspassword-512-1-JEh" src="./assets/iconspassword-512-1.png"/>
+  </div>
+</div>
+  )
+}
+
+export default SignUp
