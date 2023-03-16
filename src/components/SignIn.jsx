13a60c04fe56e3f27dc68b45d3d78e032fd1dc53
Zorous <oussamabelhadi5@gmail.com>
2023-03-16 11:54:24 +0100

    sign_in style

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function SignIn() {
   return (
@@ -7,7 +8,7 @@ function SignIn() {
     <div className="auto-group-nnzk-hnu">
       <img alt="pic" className="eba91235a2dc1787ad14305033cb2e0ec5720x720-1-Fxq" src="./assets/eba91235a2dc1787ad14305033cb2e0ec5720x720-1-Kvu.png"/>
       <p className="you-dont-have-an-account-yet--peZ">You don’t have an account yet ?</p>
-      <div className="auto-group-ydzw-u3j">Sign up</div>
+      <Link to='/signup'><div className="auto-group-ydzw-u3j">Sign up</div></Link>
     </div>
     <img alt="pic" className="vector-69-vsw" src="REPLACE_IMAGE:18:466"/>
   </div>
@@ -21,13 +22,13 @@ function SignIn() {
     <p className="password-FMo">Password</p>
     <div className="rectangle-102-9CH">
     </div>
-    <p className="sign-up-3Hf">Sign Up</p>
+    <p className="sign-up-3Hf">Sign in</p>
     <div className="rectangle-100-wP3">
     </div>
     <div className="rectangle-101-3gy">
     </div>
-    <div className="characters-minimum-x3F">8 characters minimum</div>
-    <div className="examplesitecom-DE5">Example@site.com</div>
+    <input type={"password"} className="characters-minimum-x3F text_input" placeholder='8 characters minimum' />
+    <input type={"email"} className="examplesitecom-DE5 text_input" placeholder="name@sitename.com" />
     <img alt="pic" className="item-561127-1-h9F" src="./assets/-Uqs.png"/>
     <img alt="pic" className="iconspassword-512-1-nRb" src="./assets/iconspassword-512-1-2X7.png"/>
     <p className="you-dont-have-an-account-click-here-to-sign-up-59o">
