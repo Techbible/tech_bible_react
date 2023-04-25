e182b82daccbafd94f0862f27759f76358c6a066
marwane <marwanekh1998@gmail.com>
2023-04-25 15:16:08 +0100

    Adding sign in

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { NewsList } from './components';
+import { Navigate, Route, Routes } from "react-router-dom";
+import { NewsList } from "./components";
 import { AuthContext } from "./context/AuthContext";
 
-
 import {
-  Home, Profile, SignIn, SignUp, Tools, UserList, ToolDetails
+  Home,
+  Profile,
+  SignIn,
+  SignUp,
+  Tools,
+  UserList,
+  ToolDetails,
 } from "./views";
 
-import { AddTool } from './admin';
-import { useContext } from 'react';
-import { NewsContextProvider } from './context/NewsContext';
-import { useEffect } from 'react';
-import Navbar from './components/Navbar/Navbar';
-import Socials from './components/Socials/Socials';
-import NewSignIn from './views/NewSignIn';
-
+import { AddTool } from "./admin";
+import { useContext } from "react";
+import { NewsContextProvider } from "./context/NewsContext";
+import { useEffect } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Socials from "./components/Socials/Socials";
 
 
 function App() {
-
-
   const { currentUser, isAdmin } = useContext(AuthContext);
 
   const ProtectedRoute = ({ children }) => {
@@ -29,7 +30,6 @@ function App() {
     return children;
   };
 
-
   // const CheckAdmin = ({ children }) => {
   //   console.log("from routing",isAdmin)
   //   if (!isAdmin) {
@@ -38,51 +38,56 @@ function App() {
   //   return children;
   // };
 
-
-
   return (
     <div>
-    <Navbar />
-    <Routes>
-      <Route index path="/" element=<Home /> />
-      <Route path='/signup' element=<SignUp /> />
-      <Route path='/signin' element=<SignIn /> />
-      <Route path='/profile' element=<Profile /> />
-      <Route path='/list' element=<UserList /> />
-      <Route path='/NewSignIn' element=<NewSignIn /> />
+      <Navbar />
+      <Routes>
+        <Route index path="/" element=<Home /> />
+        <Route path="/signup" element=<SignUp /> />
+        <Route path="/signin" element=<SignIn /> />
+        <Route path="/profile" element=<Profile /> />
+        <Route path="/list" element=<UserList /> />
 
-      <Route path='/tools' element={
-        <ProtectedRoute>
-          <Tools />
-        </ProtectedRoute>
-      } />
+        <Route
+          path="/tools"
+          element={
+            <ProtectedRoute>
+              <Tools />
+            </ProtectedRoute>
+          }
+        />
 
-      <Route path='/ToolDetails/:id' element={
-        <ProtectedRoute>
-          <ToolDetails />
-        </ProtectedRoute>
-      } />
+        <Route
+          path="/ToolDetails/:id"
+          element={
+            <ProtectedRoute>
+              <ToolDetails />
+            </ProtectedRoute>
+          }
+        />
 
-      <Route path='/tools/ToolDetails/:id' element={
-        <ProtectedRoute>
-          <ToolDetails />
-        </ProtectedRoute>
-      } />
+        <Route
+          path="/tools/ToolDetails/:id"
+          element={
+            <ProtectedRoute>
+              <ToolDetails />
+            </ProtectedRoute>
+          }
+        />
 
-      <Route path='/News' element={
-        <NewsContextProvider>
-          <NewsList />
-        </NewsContextProvider>
-      } />
+        <Route
+          path="/News"
+          element={
+            <NewsContextProvider>
+              <NewsList />
+            </NewsContextProvider>
+          }
+        />
 
-      <Route path='/addTool' element=<AddTool /> />
+        <Route path="/addTool" element=<AddTool /> />
 
-      <Route path='/tools' element={
-          <Tools />
-      } />
-    </Routes>
-
-
+        <Route path="/tools" element={<Tools />} />
+      </Routes>
     </div>
   );
 }
