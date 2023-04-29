28417e0e43cd2f88b285abce28042c898b8b72db
Zorous <oussamabelhadi5@gmail.com>
2023-04-29 11:40:55 +0200

    merge conflict

--- src/views/SignUp.jsx
+++ src/views/SignUp.jsx
@@ -102,7 +102,6 @@ const SignUp = () => {
           <button
             onClick={handleGoogleSignUp}
             aria-label="Continue with google"
-            role="button"
             className="focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-gray-700 py-3.5 px-4 border rounded-lg border-gray-700 flex items-center w-full mt-6"
           >
             <svg
