acb9dea51e1a4bed0adc6e3b74e9a9c9013f9cfb
TahaLoukili <tahaloukili13@gmail.com>
2023-04-29 10:46:28 +0100

    fix some bugs

--- src/views/Community.jsx
+++ src/views/Community.jsx
@@ -58,8 +58,8 @@ function Community() {
             d="M19 9l-7 7-7-7"
           />
         </svg>
-      </button>
-      {isOpen && (
+                </button>
+                  {isOpen && (
         <div
           id="dropdown"
           className="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
@@ -112,7 +112,7 @@ function Community() {
           
         </div>
         
-      )}
+                  )}
 
 
 
@@ -135,42 +135,14 @@ function Community() {
                 </form>
             </div>
 
-            <form className="ml-20">
-              <label
-                for="default-search"
-                class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-              >
-                Search
-              </label>
-              <div class="relative">
-                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                  <svg
-                    aria-hidden="true"
-                    class="w-5 h-5 text-gray-500 dark:text-gray-400"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                    ></path>
-                  </svg>
-                </div>
-                <input
-                  type="search"
-                  id="default-search"
-                  class="flex block w-full p-1 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Search discussions"
-                  required
-                />
-              </div>
-            </form>
+            <div>
+          <Comment/>
+          <Comment/>
+          <Comment/>
+        </div>
           </div>
         </main>
+       
         <aside className="sidebarWithSeparator right">
                 <p>TOPICS </p>
                 <Topic/>
@@ -187,15 +159,10 @@ function Community() {
         
       </div>
 
-        <aside className="sidebarWithSeparator bottom">
-          <p>NEW DISCUSSIONS</p>
-          <Discussion />
-          <Discussion />
-          <Discussion />
-        </aside>
-      </aside>
-    </div>
-    </div>
+       
+      
+    
+    
   );
 }
 
