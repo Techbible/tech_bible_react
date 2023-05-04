a3193b9b9381c4647f4dc3547110cbad16f1faf8
Zorous <oussamabelhadi5@gmail.com>
2023-05-04 16:21:29 +0200

    updates

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -151,8 +151,7 @@ const Home = () => {
                     ></path>
                   </svg>
                 </div>
-                <div></div>
-                <input
+                 <input
                   type="text"
                   id="voice-search"
                   className="bg-white h-[36px] border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
