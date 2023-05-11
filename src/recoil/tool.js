ab6e42bfd155bacf7d05c29b96424869b63de6f0
Zorous <oussamabelhadi5@gmail.com>
2023-05-11 16:27:22 +0200

    updates

--- src/recoil/tool.js
+++ src/recoil/tool.js
@@ -9,20 +9,8 @@ export const allToolsAtom = atom({
     key: 'allTools/default',
     get: async () => {
       try {
-        const url = 'https://data.mongodb-api.com/app/data-dcnnw/endpoint/data/v1/collections/tools';
-        const apiKey = '645ba7272cb90643036a27b1';
-      
-        axios.get(url, {
-          headers: {
-            apiKey,
-          },
-        })
-          .then((response) => {
-            console.log('Data fetched successfully:', response.data);
-          })
-          .catch((error) => {
-            console.error('Error fetching data:', error);
-          });
+        const response = await axios.get('http://localhost:5000/mongo-tools');
+        return response.data;
       } catch (error) {
         console.error(error);
         return [];
