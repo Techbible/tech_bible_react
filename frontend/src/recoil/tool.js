eb5be98dc96f3cd4bbc127a2dc4059ade909d4a4
Zorous <oussamabelhadi5@gmail.com>
2023-05-13 20:01:07 +0200

    lineclamp

--- frontend/src/recoil/tool.js
+++ frontend/src/recoil/tool.js
@@ -7,11 +7,7 @@ export const allToolsAtom = selector({
   default: [],
   get: async ({ get }) => {
     try {
-<<<<<<< HEAD
       const response = await axios.get(`${BASE_URL}/mongo-tools`);
-=======
-      let response = await axios.get("http://localhost:5000/mongo-tools");
->>>>>>> 95ad6f14d9f9e72a720240dcf82c0774649accdc
       return response.data;
     } catch (error) {
       console.error(error);
